169b79d4fe0028557cf0c80d9f861e1518dedf91
Allison Hamelin <allison.hamelin@outlook.com>
2019-03-14 20:49:28 -0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import {Nav, Navbar} from "react-bootstrap";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 import './App.css';
+import Home from "./views/Home";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar className="justify-content-center" bg="light" variant="light">
-                <Nav>
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>About</Nav.Link>
-                </Nav>
-            </Navbar>
-        </div>
+        <Router>
+            <div>
+                <Navbar className="justify-content-center" bg="light" variant="light">
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar>
+
+                <Route exact path="/" component={Home} />
+            </div>
+        </Router>
     );
 }
 
