0b08cc0e677898e27732b52284fb74e48608bae4
Allison Hamelin <allison.hamelin@outlook.com>
2019-03-17 12:46:37 -0700

    Remove react-bootstrap, add element, update nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Menu } from 'element-react';
 import Home from "./views/Home";
+import 'element-theme-default';
+import './App.css';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Navbar className="justify-content-center" bg="light" variant="light">
-                    <Nav>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                    </Nav>
-                </Navbar>
+                <Menu defaultActive="0" mode="horizontal">
+                    <Menu.Item index="0">
+                        <Link to="/">Home</Link>
+                    </Menu.Item>
+                    <Menu.Item index="1">
+                        <Link to="/about">About</Link>
+                    </Menu.Item>
+                </Menu>
 
                 <Route exact path="/" component={Home} />
             </div>
